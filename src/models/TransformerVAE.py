44e167c112ada7b23dbaf94f6be20e4e1d226897
alecnoppe <alec@genuwin.nl>
2024-06-20 02:34:52 +0200

    add: VAE template for conditional and unconditional models. add: training loop

---
+++ src/models/TransformerVAE.py
@@ -0,0 +1,12 @@
+"""
+Transformer VAE model.
+
+Combination of ResNet (down/up) blocks and Attention blocks.
+"""
+from models import *
+
+class TransformerEncoder(IEncoder):
+    pass
+
+class TransformerDecoder(IDecoder):
+    pass
---
+++ src/models/__init__.py
@@ -0,0 +1,8 @@
+from .VAE import VAE, IEncoder, IDecoder, IPrior
+
+__all__ = [
+    'VAE',
+    'IEncoder',
+    'IDecoder',
+    'IPrior',
+]
