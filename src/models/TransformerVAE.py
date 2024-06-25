aea6cf9b908a4c3ded1561bee72ffb783f20033c
Olivier Boeren <olivierboeren@live.nl>
2024-06-25 15:39:40 +0200

    add: base classes for transformer VAE

--- src/models/TransformerVAE.py
+++ src/models/TransformerVAE.py
@@ -5,8 +5,49 @@ Combination of ResNet (down/up) blocks and Attention blocks.
 """
 from src.models.VAE import VAE, IEncoder, IDecoder, IPrior
 
+
+#################
+# Encoder Block #
+#################
+
 class TransformerEncoder(IEncoder):
-    pass
+    def __init__(self):
+        super().__init__()
+
+class DownSample2D(nn.Modeule):
+    def __init__(self):
+        super().__init__()
+
+#################
+# Decoder block #
+#################
 
 class TransformerDecoder(IDecoder):
-    pass
+    def __init__(self):
+        super().__init__()
+
+
+class Upsample2D(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+##################
+# UNet mid block #
+##################
+
+class TransformerMid(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+###############
+# Base Blocks #
+###############
+
+class ResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class AttentionBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
