e6b34f6e5fac3f1c604f6fb2635841abd01ad468
Olivier Boeren <olivierboeren@live.nl>
2024-06-26 17:33:28 +0200

    add: base blocks for transformer VAE

--- src/models/TransformerVAE.py
+++ src/models/TransformerVAE.py
@@ -16,13 +16,59 @@ import numpy as np
 #################
 
 class TransformerEncoder(IEncoder):
+    @staticmethod
+    def reparameterization(mu, log_var):
+        pass
+
+    def encode(self, x, y=None):
+        pass
+
+    def sample(self, x, y=None, return_components=False):
+        pass
+
+    def log_prob(self, x, y=None, return_components=False):
+        pass
+
+    def forward(self, x, y=None):
+        pass
+
     def __init__(self):
         super().__init__()
 
 
-class DownSample2D(nn.Modeule):
-    def __init__(self):
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 layers: int = 1,
+                 kernel_size: int = 3,
+                 down_sample_factor=2.0):
         super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.layers = layers
+        self.kernel_size = kernel_size
+        self.down_sample_factor = down_sample_factor
+
+        self.res1 = ResidualBlock(in_channels, out_channels, kernel_size)
+        self.blocks = nn.ModuleList(
+            [ResidualBlock(out_channels, self.out_channels, kernel_size) for _ in range(layers - 1)])
+        self.down_sample = DownSample2D(2)
+
+    def forward(self, x):
+        x = self.res1(x)
+        for block in self.blocks:
+            x = block(x)
+        return self.down_sample(x)
+
+
+class DownSample2D(nn.Module):
+    def __init__(self, window_size=2):
+        super().__init__()
+        self.pooling = nn.AvgPool2d(kernel_size=window_size, stride=window_size)
+
+    def forward(self, x):
+        return self.pooling(x)
 
 
 #################
@@ -30,15 +76,65 @@ class DownSample2D(nn.Modeule):
 #################
 
 class TransformerDecoder(IDecoder):
-    def __init__(self):
+    def __init__(self,
+                 channel_sizes: [int],
+                 num_blocks: int,
+                 up_sample_steps,
+                 kernel_size: int = 3):
+        """
+        :param channel_sizes: The sequential number of channels
+        :param num_blocks: number of residual blocks per up sampling
+        :param kernel_size: kernel size of the residual blocks
+        """
         super().__init__()
+
+    def sample(self, z, y=None):
+        pass
+
+    def log_prob(self, x, z, y=None):
+        pass
+
+    def forward(self, x):
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 layers: int = 1,
+                 kernel_size: int = 3,
+                 up_sample_factor=2.0):
+        """
+        :param in_channels: number of input channels
+        :param out_channels: number of output channels
+        :param layers: number of residual blocks
+        :param kernel_size: kernel size for the residual blocks
+        :param up_sample_factor: factor for up sampling of the input image
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.layers = layers
+        self.kernel_size = kernel_size
+        self.up_sample_factor = up_sample_factor
+
+        self.res1 = ResidualBlock(self.in_channels, self.out_channels, self.kernel_size)
+        self.blocks = [ResidualBlock(self.out_channels, self.out_channels, self.kernel_size) for _ in range(layers - 1)]
+        self.up_sample = UpSample2D(self.out_channels, up_sample_factor=self.up_sample_factor, kernel_size=3)
+
+    def forward(self, x):
+        x = self.res1(x)
+        for block in self.blocks:
+            x = block(x)
+        return self.up_sample(x)
 
 
 class UpSample2D(nn.Module):
     # using solutions to checkerboard artifacts discussed here: https://distill.pub/2016/deconv-checkerboard/
     def __init__(self, channels, up_sample_factor=2.0, kernel_size=3):
         """
-        Upsample 2D convolution
+        Up sample 2D convolution
         :param up_sample_factor: Factor by which to up sample the input image
         """
         super().__init__()
@@ -101,7 +197,6 @@ class TransformerMid(nn.Module):
 ###############
 # Base Blocks #
 ###############
-
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3):
         """
@@ -116,8 +211,8 @@ class ResidualBlock(nn.Module):
         self.kernel_size = kernel_size
 
         # Layers
-        self.conv1 = nn.Conv2d(self.in_channels, self.out_channels, self.kernel_size, padding=self.kernel_size//2)
-        self.conv2 = nn.Conv2d(self.out_channels, self.out_channels, self.kernel_size, padding=self.kernel_size//2)
+        self.conv1 = nn.Conv2d(self.in_channels, self.out_channels, self.kernel_size, padding=self.kernel_size // 2)
+        self.conv2 = nn.Conv2d(self.out_channels, self.out_channels, self.kernel_size, padding=self.kernel_size // 2)
         self.conv_con = nn.Conv2d(self.in_channels, self.out_channels, 1)
 
     def forward(self, x):
